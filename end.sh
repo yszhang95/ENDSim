964e6e1b06ebf3ccc11b4d14023ae623606482c4
Yousen Zhang <yzhang11@bnl.gov>
2026-01-20 17:13:03 -0500

    environment update; example for streaming

--- end.sh
+++ end.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export ENDDATA=/ratpac-setup/ENDSim/install
+export ENDDATA=/ratpac-setup/ENDSim/install/share/end
 export PATH=/ratpac-setup/ENDSim/install/bin:$PATH
 export LD_LIBRARY_PATH=/ratpac-setup/ENDSim/install/lib:$LD_LIBRARY_PATH
